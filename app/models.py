042b68ff05c84dbcdefda1fda165c9e6655d9e02
Caio Silva <caiovosilva@outlook.com>
2018-09-25 16:39:54 -0300

    adicionando modelo de Fornecedores e criacao das tabelas dos modelos existentes

--- app/models.py
+++ app/models.py
@@ -56,3 +56,28 @@ class Clientes(models.Model):
 
     def __str__(self):
         return self.razaoSocial+" - "+self.cnpjCpf
+
+class Fornecedores(models.Model):
+    razaoSocial = models.TextField()
+    identificacao = models.CharField(max_length=50)
+    classificacao = models.CharField(max_length=18)
+    tipoPessoa = models.CharField(max_length=10)
+    cnpjCpf = models.CharField(max_length=15)
+    inscricaoEstadual = models.CharField(max_length=20)
+    inscricaoMunicipal = models.CharField(max_length=20)
+    endereco = models.TextField()
+    bairro = models.CharField(max_length=50)
+    municipio = models.TextField()
+    cep = models.CharField(max_length=10)
+    uf = models.CharField(max_length=2)
+    telefone = models.TextField()
+    email = models.TextField()
+    nomeTitular = models.TextField()
+    cpf = models.CharField(max_length=50)
+    funcao = models.TextField()
+
+    def save(self):
+        self.save()
+
+    def __str__(self):
+        return self.razaoSocial+" - "+self.cnpjCpf
