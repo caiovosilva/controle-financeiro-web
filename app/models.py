3300e50e2de84754a636d30a6b9ace151f8605fe
Caio Silva <caiovosilva@outlook.com>
2018-10-08 21:34:57 -0300

    alteracao de paginas

--- app/models.py
+++ app/models.py
@@ -28,9 +28,6 @@ class Empresas(models.Model):
     cpf = models.CharField(max_length=50)
     funcao = models.TextField()
 
-    # def save(self):
-    #     self.save()
-
     def __str__(self):
         return self.razaoSocial
 
