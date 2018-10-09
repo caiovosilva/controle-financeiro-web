34b91c3a83a28a588885c6a588ac51472050ad84
Caio Silva <caiovosilva@outlook.com>
2018-10-09 19:07:54 -0300

    corrigida a tela de edicao e adicao de lancamentos

--- app/urls.py
+++ app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('', views.home, name='home'),
+    path('', views.home, name='home'),
     path('', views.lancamento_receber_list, name='lancamento_receber_list'),
     path('lancamento_edit/<int:pk>/', views.lancamento_receber_edit, name='lancamento_receber_edit'),
 
--- app/views.py
+++ app/views.py
@@ -3,21 +3,22 @@ from .models import LancamentosReceber
 from django.shortcuts import get_object_or_404
 from .forms import LancamentosReceberForm
 from django.shortcuts import redirect
-# def home(request):
-#     return render(request, 'app/base.html', {})
+
+def home(request):
+    return render(request, 'app/home.html', {})
 
 def lancamento_receber_list(request):
     lancamentos = LancamentosReceber.objects.all()
     return render(request, 'app/lancamento_receber_list.html', {'lancamentos': lancamentos})
 
 def lancamento_receber_edit(request, pk):
+    lancamento = get_object_or_404(LancamentosReceber, pk=pk)
     if request.method == "POST":
-        form = LancamentosReceberForm(request.POST)
+        form = LancamentosReceberForm(request.POST, instance=lancamento)
         if form.is_valid():
             lancamento = form.save(commit=False)
             lancamento.save()
             return redirect('lancamento_receber_list')
     else:
-        form = LancamentosReceberForm()
-    lancamento = get_object_or_404(LancamentosReceber, pk=pk)
-    return render(request, 'app/lancamento_receber_edit.html', {'lancamento': lancamento, 'form': form})
+        form = LancamentosReceberForm(instance=lancamento)
+    return render(request, 'app/lancamento_receber_edit.html', {'form': form})
