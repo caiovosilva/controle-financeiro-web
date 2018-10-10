57f9727684fde29410c9f9e70f78a39a18deedd7
Caio Silva <caiovosilva@outlook.com>
2018-10-10 17:39:46 -0300

    adicionada funcionalidade de extrair um relatorio .csv dos lancamentos a receber

--- app/urls.py
+++ app/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('lancamento_edit/<int:pk>/', views.lancamento_receber_edit, name='lancamento_receber_edit'),
     path('lancamento_edit', views.lancamento_receber_edit, name='lancamento_receber_edit'),
     path('delete_lancamento_receber/<int:pk>/', views.delete_lancamento_receber, name='delete_lancamento_receber'),
+    path('relatorio_lancamentos_receber', views.relatorio_lancamentos_receber, name='relatorio_lancamentos_receber'),
 
 ]
--- app/views.py
+++ app/views.py
@@ -1,3 +1,5 @@
+import csv
+from django.http import HttpResponse
 from django.shortcuts import render
 from .models import LancamentosReceber
 from django.shortcuts import get_object_or_404
@@ -37,3 +39,15 @@ def lancamento_receber_edit(request):
 def delete_lancamento_receber(request,pk):
    u = LancamentosReceber.objects.get(pk=pk).delete()
    return redirect('lancamento_receber_list')
+
+def relatorio_lancamentos_receber(request):
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="lancamentos_a_receber.csv"'
+
+    writer = csv.writer(response)
+    writer.writerow(['Cliente', 'Empresa', 'Data de Vencimento', 'Data de Emissão', 'Valor do Titulo', 'Numero do Documento'])
+
+    for lancamento in LancamentosReceber.objects.all():
+        writer.writerow([lancamento.Cliente, lancamento.Empresa, lancamento.data_Vencimento, lancamento.data_Emissao, lancamento.valor_Titulo, lancamento.numero_Documento])
+
+    return response
